6a04b5d2c638c7ab11500e5fe8dcaf7efde2a038
Stephen Munabo <stephen.munabo@gmail.com>
2020-08-21 23:46:45 -0500

    https

--- application/config/app-config.php
+++ application/config/app-config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 * environments.
 *
 */
-define('APP_BASE_URL', 'http://office.nengu.tech/');
+define('APP_BASE_URL', 'https://office.nengu.tech/');
 
 /*
 * --------------------------------------------------------------------------
